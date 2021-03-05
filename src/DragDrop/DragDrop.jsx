2bef5d71a7281dd0f8f39410132e8844b7c77047
Ilias Azagagh <ilias.azagagh@gmail.com>
2021-03-05 12:45:20 +0100

    Upload image to firebase #60

--- src/DragDrop/DragDrop.jsx
+++ src/DragDrop/DragDrop.jsx
@@ -1,22 +1,59 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useCallback } from "react";
+/* eslint-disable */
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { storage } from "../FileUpload/FileUpload";
 
+// TODO: generiek schrijven
 export default function DragDrop() {
-  const onDrop = useCallback((acceptedFiles) => {
-    console.log(acceptedFiles);
+  const onDrop = useCallback((acceptedImage) => {
+    setImage(acceptedImage);
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
+  const [image, setImage] = useState([]);
+  const [imgUrl, setImgUrl] = useState("");
+
+  console.log(image);
+
+  const handleUpload = () => {
+    if (image.length !== 0) {
+      console.log(image[0].name);
+      const uploadTask = storage.ref(`meals/${image[0].name}`).put(image[0]);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          console.log(snapshot);
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          storage
+            .ref("meals")
+            .child(image[0].name)
+            .getDownloadURL()
+            .then((url) => {
+              setImgUrl(url);
+            });
+        }
+      );
+    }
+  };
+
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag over a files or click to select files</p>
-      )}
-      <button type="submit">Upload!</button>
+    <div>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the image here ...</p>
+        ) : (
+          <p>Drag over an image or click to select the image</p>
+        )}
+      </div>
+      <button type="submit" onClick={handleUpload}>
+        Upload!
+      </button>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,21 @@
 /* eslint-env browser */
 import React from "react";
 import ReactDOM from "react-dom";
-import app from "firebase/app";
+// import app from "firebase/app";
 import App from "./App/App";
 
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-};
+// const config = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+// };
 
-// Initialize Firebase
-app.initializeApp(config);
+// // Initialize Firebase
+// app.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
