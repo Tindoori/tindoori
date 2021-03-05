f5985e50159cde965b37ee7d35f1d0ed253ac138
Ilias Azagagh <ilias.azagagh@gmail.com>
2021-03-05 10:00:39 +0100

    Created dropzone component #60

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import firebase from "firebase/app";
+import CreateRecipe from "../CreateRecipe/CreateRecipe";
 
 function App() {
   const firebaseApp = firebase.apps[0];
   return (
     <div className="App">
+      <CreateRecipe />
       <p>Tindoori :D</p>
       <h1>React & Firebase</h1>
       <code>
