ba7d6a4b0c72c29bde7dd10661c086314ee59b37
Ilias Azagagh <ilias.azagagh@gmail.com>
2021-03-12 10:57:13 +0100

    Show preview images #60

--- src/DragDrop/DragDrop.jsx
+++ src/DragDrop/DragDrop.jsx
@@ -1,24 +1,79 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useCallback, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, ProgressBar, Button } from "react-bootstrap";
 import { useDropzone } from "react-dropzone";
 import { storage } from "../FileUpload/FileUpload";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const thumbsContainer = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  marginTop: 16,
+};
+
+const thumb = {
+  display: "inline-flex",
+  borderRadius: 2,
+  border: "1px solid #eaeaea",
+  marginBottom: 8,
+  marginRight: 8,
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: "border-box",
+};
+
+const thumbInner = {
+  display: "flex",
+  minWidth: 0,
+  overflow: "hidden",
+};
+
+const img = {
+  display: "block",
+  width: "auto",
+  height: "100%",
+};
+
 // TODO: generiek schrijven
 export default function DragDrop() {
-  const [image, setImage] = useState([]);
+  const [files, setFiles] = useState([]);
   const [imgUrl, setImgUrl] = useState("");
   const [progress, setProgress] = useState(0);
 
-  const onDrop = useCallback((acceptedImage) => {
-    setImage(acceptedImage);
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const thumbs = files.map((file) => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img src={file.preview} style={img} alt="" />
+      </div>
+    </div>
+  ));
+
+  useEffect(
+    () => () => {
+      // Make sure to revoke the data uris to avoid memory leaks
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [files]
+  );
 
   const handleUpload = () => {
-    if (image.length !== 0) {
-      const uploadTask = storage.ref(`meals/${image[0].name}`).put(image[0]);
+    if (files.length !== 0) {
+      const uploadTask = storage.ref(`meals/${files[0].name}`).put(files[0]);
       uploadTask.on(
         "state_changed",
         (snapshot) => {
@@ -33,7 +88,7 @@ export default function DragDrop() {
         () => {
           storage
             .ref("meals")
-            .child(image[0].name)
+            .child(files[0].name)
             .getDownloadURL()
             .then((url) => {
               // Url of the uploaded image
@@ -56,9 +111,8 @@ export default function DragDrop() {
               ? "Drop the image here ..."
               : "Drag over an image or click to select the image"}
           </Card.Text>
-          <Button type="submit" onClick={handleUpload}>
-            Upload
-          </Button>
+          <aside style={thumbsContainer}>{thumbs}</aside>
+          <Button onClick={handleUpload}>Upload</Button>
         </Card.Body>
         <ProgressBar now={progress} />
       </Card>
