f2f046be2330f281fca86c0512a4ca51e33f0ffc
Tonny <mrtonnysu@gmail.com>
2021-03-17 13:52:31 +0100

    Added Card component to App.jsx. #76

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import firebase from "firebase/app";
+import Card from "../Card/Card";
 
 function App() {
   const firebaseApp = firebase.apps[0];
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <p>Tindoori :D</p>
       <h1>React & Firebase</h1>
+      <Card />
       <code>
         <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
       </code>
