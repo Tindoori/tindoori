6eae116812c89d38adc94b1515e32769d669513b
Tonny <mrtonnysu@gmail.com>
2021-03-17 17:46:29 +0100

    Revert "Feature/login"

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import firebase from "firebase/app";
-import Login from "../Login/Login";
-import Home from "../Home/Home";
-import { AuthProvider } from "../Auth";
-import PrivateRoute from "../PrivateRoute";
 
 function App() {
   const firebaseApp = firebase.apps[0];
-  console.log(firebaseApp);
   return (
-    <AuthProvider>
-      <Router>
-        <PrivateRoute exact path="/" component={Home} />
-        <Route exact path="/Login" component={Login} />
-      </Router>
-    </AuthProvider>
+    <div className="App">
+      <p>Tindoori :D</p>
+      <h1>React & Firebase</h1>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code>
+    </div>
   );
 }
 
