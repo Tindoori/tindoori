43262f5712a73534555315aa86a802093f04e83b
negam00 <negam00@github.com>
2021-03-17 16:15:19 +0100

    added Bootstrap #24

--- src/Match/Match.jsx
+++ src/Match/Match.jsx
@@ -1,39 +1,24 @@
 import { React, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 export default function Match() {
-  // eslint-disable-next-line no-unused-vars
-  const [match, setMatch] = useState(true);
+  const [showModal, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
   return (
-    <div className="container">
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
+    <>
+      <Button variant="primary" onClick={handleShow}>
         Launch
-      </button>
-      <div className="modal hide">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Its a match!</h5>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body"> recipe content</div>
-          </div>
-        </div>
-      </div>
-    </div>
+      </Button>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Its a match!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body> recipe content</Modal.Body>
+      </Modal>
+    </>
   );
 }
