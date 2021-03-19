d42d9a0e89b66229dcb45dbef46a122580129a1e
negam00 <negam00@github.com>
2021-03-19 09:39:19 +0100

    removed console log  from App.jsx #69 #70

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import firebase from "firebase/app";
 import Login from "../Login/Login";
 import Home from "../Home/Home";
 import { AuthProvider } from "../Auth";
 import PrivateRoute from "../PrivateRoute";
 
 function App() {
-  const firebaseApp = firebase.apps[0];
-  console.log(firebaseApp);
   return (
     <AuthProvider>
       <Router>
