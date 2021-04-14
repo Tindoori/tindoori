50254749fe400083ca72c5bc6d05ca685eb784c5
negam00 <negam00@github.com>
2021-04-14 10:20:47 +0200

    changed to mobile first #24

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import TinderCard from "react-tinder-card";
+import Match from "../Match/Match";
 
 export default function Card() {
+  const [liked, setLiked] = useState(false);
+
   const onSwipe = (direction) => {
     // TODO: Replace with a call to the database
     console.log(`you swiped: ${direction}`);
+    if (direction === "right") {
+      setLiked(true);
+    }
   };
 
   return (
-    <TinderCard onSwipe={onSwipe} preventSwipe={["up", "down"]}>
-      Card
-    </TinderCard>
+    <>
+      <TinderCard onSwipe={onSwipe} preventSwipe={["up", "down"]}>
+        Card
+      </TinderCard>
+      {liked === true && <Match />}
+    </>
   );
 }
--- src/Match/Match.jsx
+++ src/Match/Match.jsx
@@ -1,28 +1,25 @@
 import { React, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Match.css";
-import { Button, Modal } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import RecipeCard from "../RecipeCard/RecipeCard";
 
 export default function Match() {
-  const [showModal, setShow] = useState(false);
+  const [showModal, setShow] = useState(true);
 
-  const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch
-      </Button>
-      <Modal show={showModal} onHide={handleClose} size="lg">
-        <Modal.Header closeButton>
-          <Modal.Title>Its a match!</Modal.Title>
+      <Modal id="match-modal" show={showModal} onHide={handleClose} size="lg">
+        <Modal.Header id="match-modal-header" closeButton>
+          <Modal.Title id="match-modal-header-title">Its a match!</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          content
-          <RecipeCard />
-          <h5>The recipe will be added to your recipe overview!</h5>
+        <Modal.Body id="match-modal-body">
+          <RecipeCard id="match-modal-recipe-card" />
+          <h5 id="match-modal-footer">
+            The recipe will be added to your recipe overview!
+          </h5>
         </Modal.Body>
       </Modal>
     </>
