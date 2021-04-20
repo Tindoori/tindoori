ba5d43bcc9297adc41cdbae4d78caba35d590592
negam00 <negam00@github.com>
2021-04-20 13:43:45 +0200

    removed fragment #86

--- src/Registration/Registration.jsx
+++ src/Registration/Registration.jsx
@@ -61,82 +61,80 @@ export default function Registration() {
   };
 
   return (
-    <>
-      <Card id="registration-card">
-        <Card.Title>Create an account</Card.Title>
-        <Form className="registration-form" onSubmit={handleSubmit}>
-          <Form.Group id="registration-form-group" controlId="formDisplayName">
-            <Form.Label>Full Name:</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="E.g: Joe Johnson"
-              onChange={handleChange}
-              name="displayName"
-              required
-            />
-          </Form.Group>
-          <Form.Group id="registration-form-group" controlId="formEmail">
-            <Form.Label>Email address:</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="E.g: joe123@gmail.com"
-              className="email"
-              onChange={handleChange}
-              name="email"
-              required
-            />
-          </Form.Group>
-          <Form.Group id="registration-form-group" controlId="formPassword">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Your Password"
-              className="password"
-              onChange={handleChange}
-              name="password"
-              required
-              min={6}
-            />
-            <Form.Text id="passwordHelpBlock" muted>
-              Your password must be at least 6 characters long and must not
-              contain spaces or emoji.
-            </Form.Text>
-          </Form.Group>
+    <Card id="registration-card">
+      <Card.Title>Create an account</Card.Title>
+      <Form className="registration-form" onSubmit={handleSubmit}>
+        <Form.Group id="registration-form-group" controlId="formDisplayName">
+          <Form.Label>Full Name:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="E.g: Joe Johnson"
+            onChange={handleChange}
+            name="displayName"
+            required
+          />
+        </Form.Group>
+        <Form.Group id="registration-form-group" controlId="formEmail">
+          <Form.Label>Email address:</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="E.g: joe123@gmail.com"
+            className="email"
+            onChange={handleChange}
+            name="email"
+            required
+          />
+        </Form.Group>
+        <Form.Group id="registration-form-group" controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your Password"
+            className="password"
+            onChange={handleChange}
+            name="password"
+            required
+            min={6}
+          />
+          <Form.Text id="passwordHelpBlock" muted>
+            Your password must be at least 6 characters long and must not
+            contain spaces or emoji.
+          </Form.Text>
+        </Form.Group>
 
-          <Form.Group
-            id="registration-form-group"
-            controlId="formConfirmPassword"
-          >
-            <Form.Label>Password confirmation:</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Confirm Password"
-              onChange={handleChange}
-              name="confirmPassword"
-              required
-              min={6}
-            />
-          </Form.Group>
-          {user.error && (
-            <Alert id="registration-form-alert" variant="danger">
-              {user.error}
-            </Alert>
-          )}
-          {createdState && (
-            <Alert id="registration-form-alert" variant="info">
-              You have created an account!
-            </Alert>
-          )}
-          <Button
-            id="registration-form-button"
-            variant="danger"
-            type="submit"
-            block
-          >
-            Create account
-          </Button>
-        </Form>
-      </Card>
-    </>
+        <Form.Group
+          id="registration-form-group"
+          controlId="formConfirmPassword"
+        >
+          <Form.Label>Password confirmation:</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            onChange={handleChange}
+            name="confirmPassword"
+            required
+            min={6}
+          />
+        </Form.Group>
+        {user.error && (
+          <Alert id="registration-form-alert" variant="danger">
+            {user.error}
+          </Alert>
+        )}
+        {createdState && (
+          <Alert id="registration-form-alert" variant="info">
+            You have created an account!
+          </Alert>
+        )}
+        <Button
+          id="registration-form-button"
+          variant="danger"
+          type="submit"
+          block
+        >
+          Create account
+        </Button>
+      </Form>
+    </Card>
   );
 }
