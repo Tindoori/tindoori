3ca60fcdb14a9775689455a62ef877f2321ba76f
Ilias Azagagh <ilias.azagagh@gmail.com>
2021-05-13 12:54:16 +0200

    Added jsdoc #57

--- src/CustomDropdown/CustomDropdown.jsx
+++ src/CustomDropdown/CustomDropdown.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { DropdownButton, Dropdown } from "react-bootstrap";
 
+/**
+ * Creates dropdowns with data provides.
+ * @param   {array} keys    The name(s) of the dropdown(s).
+ * @param   {array} values  The values for the options.
+ * @return  {<type>}        Returns a dropdown menu with options.
+ */
 export default function CustomDropdown({ keys, values }) {
   return Object.values(keys).map((key, i) => {
     return (
