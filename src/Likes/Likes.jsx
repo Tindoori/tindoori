fb79e885042eef9df372e80ae2356ee35c06dd99
Tonny <mrtonnysu@gmail.com>
2021-05-13 15:28:23 +0200

    Fetch liked recipe objects. #28

--- src/Likes/Likes.jsx
+++ src/Likes/Likes.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import firebase from "firebase";
 import NavBar from "../NavBar/NavBar";
+import {ListGroup} from "react-bootstrap";
 
 //  TODO make route private
 export default function Likes() {
   const fs = firebase.firestore();
   const auth = firebase.auth();
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     let likedRecipes = [""];
@@ -19,18 +22,39 @@ export default function Likes() {
         .doc("recipes")
         .get()
         .then((snapshot) => {
+          // likedRecipes.push(snapshot.data().likes);
           likedRecipes = snapshot.data().likes;
         });
     }
 
     fetchLikesData().then(() => {
-      console.log(likedRecipes);
+      fs.collection("recipe")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+              if (likedRecipes.length !== 0) {
+                  // Filter liked recipes
+                  if (likedRecipes.includes(doc.id)) {
+                      setRecipes((prevState) => [...prevState, doc.data()]);
+                  }
+              }
+          });
+        });
     });
   }, [fs, auth.currentUser.uid]);
+
+
+    console.log(recipes);
 
   return (
     <>
       <NavBar />
+        <div id="liked-recipes-list">
+            <ListGroup>
+                {/*{recipes.map((recipe) => (*/}
+                {/*))}*/}
+            </ListGroup>
+        </div>
     </>
   );
 }
