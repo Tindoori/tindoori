af25144f5164165686562818349da4a32e9078d9
Tonny <mrtonnysu@gmail.com>
2021-05-13 17:31:54 +0200

    Liked recipes are shown in listgroup. #28

--- src/Likes/Likes.jsx
+++ src/Likes/Likes.jsx
@@ -1,9 +1,9 @@
-/* eslint-disable */
 import React, { useEffect, useState } from "react";
+import { Card, ListGroup } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Likes.css";
 import firebase from "firebase";
 import NavBar from "../NavBar/NavBar";
-import {ListGroup} from "react-bootstrap";
 
 //  TODO make route private
 export default function Likes() {
@@ -12,7 +12,7 @@ export default function Likes() {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    let likedRecipes = [""];
+    let likedRecipes = [];
 
     async function fetchLikesData() {
       await fs
@@ -22,7 +22,7 @@ export default function Likes() {
         .doc("recipes")
         .get()
         .then((snapshot) => {
-          // likedRecipes.push(snapshot.data().likes);
+          //    TODO fix bug where no likes causes error
           likedRecipes = snapshot.data().likes;
         });
     }
@@ -32,29 +32,38 @@ export default function Likes() {
         .get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
-              if (likedRecipes.length !== 0) {
-                  // Filter liked recipes
-                  if (likedRecipes.includes(doc.id)) {
-                      setRecipes((prevState) => [...prevState, doc.data()]);
-                  }
+            if (likedRecipes.length !== 0) {
+              // Filter liked recipes
+              if (likedRecipes.includes(doc.id)) {
+                setRecipes((prevState) => [...prevState, doc.data()]);
               }
+            }
           });
         });
     });
   }, [fs, auth.currentUser.uid]);
 
-
-    console.log(recipes);
+  console.log(recipes);
 
   return (
     <>
       <NavBar />
-        <div id="liked-recipes-list">
-            <ListGroup>
-                {/*{recipes.map((recipe) => (*/}
-                {/*))}*/}
-            </ListGroup>
-        </div>
+      <ListGroup id="liked-recipe-list">
+        {recipes.map((recipe) => (
+          <Card id="liked-recipe-card">
+            <Card.Img id="recipe-img" src={recipe.imgPath} />
+            <Card.Body>
+              <Card.Title>{recipe.name}</Card.Title>
+              <Card.Text className="recipe-description">
+                {recipe.description}
+              </Card.Text>
+              <Card.Text className="recipe-cook-time">
+                {recipe.cookingTime} min
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </ListGroup>
     </>
   );
 }
