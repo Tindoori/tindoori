3bc5c56c2632be62ede02e5d859e2068e6968074
Ilias Azagagh <ilias.azagagh@gmail.com>
2021-05-13 15:17:16 +0200

    Renamed customdropdown component #57

--- src/FormDropdown/FormDropdown.jsx
+++ src/FormDropdown/FormDropdown.jsx
@@ -8,10 +8,10 @@ import { Form } from "react-bootstrap";
  * @param   {array} values  The values for the options within the dropdown.
  * @return  {Dropdown}        Returns a dropdown menu with options.
  */
-export default function CustomDropdown({ name, values }) {
+export default function FormDropdown({ name, values }) {
   // For the keys, all the spaces are replaces with dashes.
   return (
-    <Form.Control as="select" defaultValue={name} name={name}>
+    <Form.Control as="select" name={name}>
       {/* Default option when the user doesn't choose a value */}
       <option key={`${name}-dropdown-item-default`}> </option>
       {Object.values(values).map((value) => (
