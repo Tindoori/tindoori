55f86b6e2a75141f774895caeabf5931a1013408
Ilias Azagagh <ilias.azagagh@gmail.com>
2021-05-13 13:53:15 +0200

    Added default option to dropdown #57

--- src/CustomDropdown/CustomDropdown.jsx
+++ src/CustomDropdown/CustomDropdown.jsx
@@ -6,12 +6,14 @@ import { Form } from "react-bootstrap";
  * Creates dropdowns with data provides.
  * @param   {string} name    The name of the dropdown.
  * @param   {array} values  The values for the options within the dropdown.
- * @return  {<DropdownButton>}        Returns a dropdown menu with options.
+ * @return  {Dropdown}        Returns a dropdown menu with options.
  */
 export default function CustomDropdown({ name, values }) {
   // For the keys, all the spaces are replaces with dashes.
   return (
     <Form.Control as="select" defaultValue={name} name={name}>
+      {/* Default option when the user doesn't choose a value */}
+      <option key={`${name}-dropdown-item-default`}> </option>
       {Object.values(values).map((value) => (
         <option key={`${name}-dropdown-item-${value.replace(/\s+/g, "-")}`}>
           {value}
