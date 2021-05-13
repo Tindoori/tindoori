19145b25d51830c9fdc451dd4d6dab209fdfd21f
negam00 <negam00@github.com>
2021-05-13 11:30:33 +0200

    Added allergy preference to the feed page #44

--- src/Feed/Feed.jsx
+++ src/Feed/Feed.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import firebase from "firebase";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 import RecipeCard from "../RecipeCard/RecipeCard";
 import "./Feed.css";
 
@@ -7,6 +8,12 @@ export default function Feed() {
   const fs = firebase.firestore();
   const auth = firebase.auth();
   const [recipes, setRecipes] = useState([]);
+  const [allergies] = useState([
+    { label: "Sea food", value: "sea-food" },
+    { label: "Nuts", value: "nuts" },
+    { label: "Dairy", value: "dairy" },
+  ]);
+  const [selected, setSelected] = useState("nuts");
 
   useEffect(() => {
     let recipesToFilter = [""];
@@ -30,19 +37,44 @@ export default function Feed() {
     fetchConsumerData().then(() =>
       fs
         .collection("recipe")
-        .where("id", "not-in", recipesToFilter)
+        .where("allergies", "!=", selected)
         .get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
-            // Stores all recipes from database
-            setRecipes((prevState) => [...prevState, doc.data()]);
+            // Filter liked/disliked recipes
+            if (!recipesToFilter.includes(doc.id)) {
+              setRecipes((prevState) => [...prevState, doc.data()]);
+            }
           });
         })
     );
-  }, [fs, auth.currentUser.uid]);
+  }, [fs, auth.currentUser.uid, selected]);
+
+  const onPreferencesSelect = (selectedAllergy) => {
+    setSelected(selectedAllergy);
+  };
+  const Preferences = allergies.map((allergy) => (
+    <Dropdown.Item
+      id="preferences-allergies-dropdown-item"
+      key={allergy.label}
+      as="button"
+      onSelect={() => onPreferencesSelect(allergy.value)}
+    >
+      {allergy.label}
+    </Dropdown.Item>
+  ));
 
   return (
     <div id="feed">
+      <div id="preferences-allergies">
+        <DropdownButton
+          id="preferences-allergies-dropdown"
+          title="Allergies"
+          variant="danger"
+        >
+          {Preferences}
+        </DropdownButton>
+      </div>
       <div id="card-container">
         {recipes.map((recipe) => (
           <RecipeCard recipe={recipe} key={recipe.id} />
