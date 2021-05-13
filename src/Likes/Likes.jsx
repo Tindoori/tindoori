048614494d493ab69f1e6f7adf10bbdcf074f27b
Tonny <mrtonnysu@gmail.com>
2021-05-13 14:11:07 +0200

    Added Likes.css and fetches liked recipe id's. #28

--- src/Likes/Likes.jsx
+++ src/Likes/Likes.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import firebase from "firebase";
 import NavBar from "../NavBar/NavBar";
 
 //  TODO make route private
 export default function Likes() {
+  const fs = firebase.firestore();
+  const auth = firebase.auth();
+
+  useEffect(() => {
+    let likedRecipes = [""];
+
+    async function fetchLikesData() {
+      await fs
+        .collection("consumer")
+        .doc(auth.currentUser.uid)
+        .collection("recipes")
+        .doc("recipes")
+        .get()
+        .then((snapshot) => {
+          likedRecipes = snapshot.data().likes;
+        });
+    }
+
+    fetchLikesData().then(() => {
+      console.log(likedRecipes);
+    });
+  }, [fs, auth.currentUser.uid]);
+
   return (
     <>
       <NavBar />
