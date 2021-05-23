86b67843df76f4c8c9e28e1d81c777961229b710
Unknown <sohabas0021@hva.nl>
2021-05-23 16:57:44 +0200

    Fixed the console warning

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,11 +4,11 @@ import { Switch } from "react-router";
 import Login from "../Login/Login";
 import Home from "../Home/Home";
 import { AuthProvider } from "../Auth";
-import PrivateRoute from "../PrivateRoute";
 import CreateRecipe from "../CreateRecipe/CreateRecipe";
 import Registration from "../Registration/Registration";
 import Navbar from "../NavBar/NavBar";
 import Likes from "../Likes/Likes";
+import PrivateRoute from "../PrivateRoute";
 
 function App() {
   return (
@@ -17,12 +17,12 @@ function App() {
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/registration" component={Registration} />
-          <div>
+          <>
             <Navbar />
             <PrivateRoute exact path="/" component={Home} />
             <PrivateRoute exact path="/add-recipe" component={CreateRecipe} />
             <PrivateRoute exact path="/likes" component={Likes} />
-          </div>
+          </>
         </Switch>
       </Router>
     </AuthProvider>
