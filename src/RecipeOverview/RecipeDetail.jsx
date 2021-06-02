094e5c9dfe48a158ba6beff04a4ef3b8e630a099
Unknown <sohabas0021@hva.nl>
2021-06-02 14:07:04 +0200

    Added the reactflip library and also added steps to recipedetails #3
    On create recipe page i made the steps store in a array.

--- src/RecipeOverview/RecipeDetail.jsx
+++ src/RecipeOverview/RecipeDetail.jsx
@@ -1,14 +1,17 @@
+/* eslint-disable */
 import React, { useEffect, useState } from "react";
 import "firebase/firestore";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, ListGroupItem, ListGroup } from "react-bootstrap";
 import firebase from "firebase";
 import * as PropTypes from "prop-types";
+import ReactCardFlip from "react-card-flip";
 
 export default function RecipeDetail({ match }) {
   const [recipe, setRecipe] = useState();
   const recipeid = match.params.id;
   const db = firebase.firestore();
+  const [isFlipped, setIsFlipped] = useState(false);
 
   useEffect(() => {
     db.collection("recipe")
@@ -19,49 +22,67 @@ export default function RecipeDetail({ match }) {
       });
   }, [db, recipeid]);
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setIsFlipped((prevState) => ({ isFlipped: !prevState }));
+  };
+
   return (
     <>
       <br />
       {recipe && (
-        <Card id="recipe-card">
-          <Card.Img id="recipe-img" src={recipe.imgPath} />
-          <Card.Body>
-            <Card.Title>{recipe.name}</Card.Title>
-            <Card.Text className="recipe-description">
-              {recipe.description}
-            </Card.Text>
-            <Card.Header>Ingredients</Card.Header>
-            <ListGroup variant="flush">
-              {recipe.ingredients.map((ingredient) => (
-                <ListGroupItem>{ingredient}</ListGroupItem>
-              ))}
-            </ListGroup>
-            <br />
-            <ListGroupItem>
-              <Card.Text className="recipe-description">
-                Allergies: {recipe.allergies}
-              </Card.Text>
-            </ListGroupItem>
-            <ListGroupItem>
-              <Card.Text className="recipe-description">
-                dietPreference: {recipe.dietPreference}
-              </Card.Text>
-            </ListGroupItem>
-            <ListGroupItem>
-              <Card.Text className="recipe-description">
-                MealType: {recipe.mealType}
-              </Card.Text>
-            </ListGroupItem>
-            <ListGroupItem>
-              <Card.Text>Cuisine: {recipe.cuisine}</Card.Text>
-            </ListGroupItem>
-            <ListGroupItem>
-              <Card.Text className="recipe-cook-time">
-                Cooking time: {recipe.cookingTime} min
-              </Card.Text>
-            </ListGroupItem>
-          </Card.Body>
-        </Card>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          <div onClick={() => setIsFlipped((prev) => !prev)}>
+            <Card id="recipe-card">
+              <Card.Img id="recipe-img" src={recipe.imgPath} />
+              <Card.Body>
+                <Card.Title>{recipe.name}</Card.Title>
+                <Card.Text className="recipe-description">
+                  {recipe.description}
+                </Card.Text>
+                <ListGroupItem>
+                  <Card.Text className="recipe-description">
+                    Allergies: {recipe.allergy}
+                  </Card.Text>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Card.Text className="recipe-description">
+                    dietPreference: {recipe.dietary}
+                  </Card.Text>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Card.Text className="recipe-description">
+                    MealType: {recipe.mealtype}
+                  </Card.Text>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Card.Text className="recipe-cook-time">
+                    Cooking time: {recipe.cookingTime} min
+                  </Card.Text>
+                </ListGroupItem>
+              </Card.Body>
+            </Card>
+          </div>
+          <div onClick={() => setIsFlipped((prev) => !prev)}>
+            <Card>
+              <Card.Body>
+                <Card.Header>Ingredients</Card.Header>
+                <ListGroup variant="flush">
+                  {recipe.ingredients.map((ingredient) => (
+                    <ListGroupItem>{ingredient}</ListGroupItem>
+                  ))}
+                </ListGroup>
+                <br />
+                <Card.Header>Steps</Card.Header>
+                <ListGroup variant="flush">
+                  {recipe.steps.map((steps) => (
+                    <ListGroupItem>{steps}</ListGroupItem>
+                  ))}
+                </ListGroup>
+              </Card.Body>
+            </Card>
+          </div>
+        </ReactCardFlip>
       )}
     </>
   );
