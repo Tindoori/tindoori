3bf622a25974db03a75d8a7f3fe3f55ca42dd41a
Ilias Azagagh <ilias.azagagh@gmail.com>
2021-06-04 14:47:04 +0200

    Added placeholdercard component #131

---
+++ src/components/PlaceholderCard/PlaceholderCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import PropTypes from "prop-types";
+import "./PlaceholderCard.css";
+
+// Pass on placeholder text
+export default function PlaceholderCard({ cardTitle, cardBody }) {
+  return (
+    <Card id="placeholder-card">
+      <Card.Body>
+        <Card.Title>{cardTitle}</Card.Title>
+        <Card.Text>{cardBody}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+PlaceholderCard.propTypes = {
+  cardTitle: PropTypes.string.isRequired,
+  cardBody: PropTypes.string.isRequired,
+};
